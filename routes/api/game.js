bc1101642e53693a1a1d0bc75c00aaa55ebfa895
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-09-27 20:41:38 +0200

    Add simple crud routes for games

---
+++ routes/api/game.js
@@ -0,0 +1,19 @@
+const { Game } = require('../../lib/Game');
+
+module.exports = (router) => {
+	router.get('/games', (req, res, next) => {
+		const games = Game.all();
+		return res.json(games.map((game) => game.toObject()));
+	});
+
+	router.put('/games', (req, res, next) => {
+		const game = new Game(req.body);
+		game.save();
+		return res.json(game.toObject());
+	});
+
+	router.get('/games/:pid', (req, res, next) => {
+		const game = Game.getByPid(req.params.pid);
+		return res.json(game);
+	});
+};
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,15 +3,19 @@ const express = require('express');
 const { Board } = require('../../lib/Board');
 const { BoardFactory } = require('../../lib/BoardFactory');
 
+const initGameRoutes = require('./game');
+
 const router = express.Router();
+initGameRoutes(router);
+
 router.get('/board', (req, res) => {
 	res.json(BoardFactory.toObject(new Board()));
 });
 
 // test route
 router.post('/board', (req, res) => {
-	const foo = req.body.body
-	return res.json({ foo })
+	const foo = req.body.body;
+	return res.json({ foo });
 });
 
 module.exports = router;
