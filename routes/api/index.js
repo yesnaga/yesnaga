88c47c23077fb254d4718fa5149a235b46c6e7c8
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-09-28 22:59:55 +0200

    Add API error handling

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { Board } = require('../../lib/Board');
 const { BoardFactory } = require('../../lib/BoardFactory');
 
+const { ApplicationError } = require('../../lib/Errors');
+
 const initGameRoutes = require('./game');
 
 const router = express.Router();
@@ -16,6 +18,24 @@ router.get('/board', (req, res) => {
 router.post('/board', (req, res) => {
 	const foo = req.body.body;
 	return res.json({ foo });
+});
+
+router.use((err, req, res, next) => {
+	if (err instanceof ApplicationError) {
+		res.status(err.status()).json({
+			error: err.constructor.name,
+			message: err.message,
+			status: err.status(),
+			request: {
+				method: req.method,
+				route: req.route.path,
+				params: req.params,
+				data: req.body,
+			},
+		});
+	} else {
+		next(err);
+	}
 });
 
 module.exports = router;
