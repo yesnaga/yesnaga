0dc824c07ce67e73d46d5a8e4c43d10a8c93561e
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-10-03 17:38:07 +0200

    Add moveDisc use case and API

--- routes/api/move.js
+++ routes/api/move.js
@@ -14,4 +14,18 @@ module.exports = (router) => {
 			return next(err);
 		}
 	});
+
+	router.post('/moveDisc', (req, res, next) => {
+		try {
+			const { pid } = res.locals;
+			const game = Game.getByPid(pid);
+			game.moveDisc({
+				from: req.body.from,
+				to: req.body.to,
+			});
+			return res.json(game.toObject());
+		} catch (err) {
+			return next(err);
+		}
+	});
 };
