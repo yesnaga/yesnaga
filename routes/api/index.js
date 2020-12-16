728da5fc1d782614d9e42c9d7985192032f84ae9
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-12-16 15:17:41 +0100

    Simplify error handling

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const OpenApiValidator = require('express-openapi-validator');
 
-const { ApplicationError } = require('../../lib/Errors');
-
-const { Game } = require('../../lib/Game')();
-
 const initGameRoutes = require('./game');
 const initMoveRoutes = require('./move');
 
@@ -22,24 +18,20 @@ initGameRoutes(router);
 initMoveRoutes(router);
 
 router.use((err, req, res, next) => {
-	if (err instanceof ApplicationError) {
-		res.status(err.status()).json({
-			error: err.constructor.name,
-			message: err.message,
-			status: err.status(),
-			request: {
-				method: req.method,
-				route: req.route.path,
-				params: req.params,
-				data: req.body,
-			},
-		});
-	} else {
-		res.status(err.status || 500).json({
-			message: err.message,
-			errors: err.errors,
-		});
-	}
+	const error = err.errors || err.constructor.name;
+	const message = err.message || 'Internal Server Error';
+	const status = err.status || 500;
+	res.status(status).json({
+		error,
+		message,
+		status,
+		request: {
+			method: req.method,
+			route: (req.route || {}).path,
+			params: req.params,
+			data: req.body,
+		},
+	});
 });
 
 module.exports = router;
